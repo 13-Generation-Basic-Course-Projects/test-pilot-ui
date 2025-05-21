0dd59af898d49a5a5600cdb9fc1878e05915f38f
nethmesaboth <nethmesaboth10@gmail.com>
2025-05-21 21:19:48 +0700

    fix(sidebar): fix sidebar padding

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -387,10 +387,7 @@ function SidebarGroup({ className, ...props }: React.ComponentProps<"div">) {
 		<div
 			data-slot="sidebar-group"
 			data-sidebar="group"
-			className={cn(
-				"relative flex w-full min-w-0 flex-col p-2 px-4",
-				className
-			)}
+			className={cn("relative flex w-full min-w-0 flex-col p-2 ", className)}
 			{...props}
 		/>
 	);
@@ -459,7 +456,7 @@ function SidebarMenu({ className, ...props }: React.ComponentProps<"ul">) {
 		<ul
 			data-slot="sidebar-menu"
 			data-sidebar="menu"
-			className={cn("flex w-full min-w-0 flex-col gap-1", className)}
+			className={cn("flex w-full flex-col gap-1", className)}
 			{...props}
 		/>
 	);
