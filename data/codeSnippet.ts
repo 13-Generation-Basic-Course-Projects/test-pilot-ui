bf4875fb3c43a926d2e2f81c8b09a6e1f53056ef
K3yyy <yunmenghuy33@gmail.com>
2025-05-23 09:14:49 +0700

    feat(code-snippet): create code snippet ui

---
+++ data/codeSnippet.ts
@@ -0,0 +1,104 @@
+const codeSnippets = [
+    {
+        language: "javascript",
+        label: "JavaScript",
+        code: `
+function greet() {
+  console.log('Hello JavaScript!');
+}
+    `,
+    },
+    {
+        language: "python",
+        label: "Python",
+        code: `
+def greet():
+    print("Hello Python!")
+    `,
+    },
+    {
+        language: "java",
+        label: "Java",
+        code: `
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello Java!");
+  }
+}
+    `,
+    },
+    {
+        language: "csharp",
+        label: "C#",
+        code: `
+using System;
+class Program {
+  static void Main() {
+    Console.WriteLine("Hello C#!");
+  }
+}
+    `,
+    },
+    {
+        language: "html",
+        label: "HTML",
+        code: `
+<!DOCTYPE html>
+<html lang="">
+  <head><title>Hello HTML</title></head>
+  <body>Hello World!</body>
+</html>
+    `,
+    },
+    {
+        language: "typescript",
+        label: "TypeScript",
+        code: `
+function greet(): void {
+  console.log("Hello TypeScript!");
+}
+    `,
+    },
+    {
+        language: "css",
+        label: "CSS",
+        code: `
+body {
+  background-color: #f0f0f0;
+  color: #333;
+}
+    `,
+    },
+    {
+        language: "go",
+        label: "Go",
+        code: `
+package main
+import "fmt"
+func main() {
+  fmt.Println("Hello Go!")
+}
+    `,
+    },
+    {
+        language: "php",
+        label: "PHP",
+        code: `
+<?php
+echo "Hello PHP!";
+?>
+    `,
+    },
+    {
+        language: "ruby",
+        label: "Ruby",
+        code: `
+def greet
+  puts "Hello Ruby!"
+end
+
+greet
+    `,
+    },
+];
+export default codeSnippets;
