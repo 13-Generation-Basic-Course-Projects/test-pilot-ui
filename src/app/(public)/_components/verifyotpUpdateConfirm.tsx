1462bf5e24b4d9c37103c1cb5a69c3f33c77edb1
Pon Channarith <ponchannarith@gmail.com>
2025-05-25 16:48:01 +0700

    update-password form

---
+++ src/app/(public)/_components/verifyotpUpdateConfirm.tsx
@@ -0,0 +1,39 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import GithubIcon from "@/components/icons/github";
+import GoogleIcon from "@/components/icons/google";
+export function VerifyotpUpdateConfirmForm({
+	className,
+	...props
+}: React.ComponentPropsWithoutRef<"form">) {
+	return (
+		<form className={cn("flex flex-col gap-8 mb-10", className)} {...props}>
+			<div className="flex flex-col items-center gap-2 text-center">
+				<h1 className="text-2xl font-bold">Enter New Password</h1>
+				<p className="text-balance text-sm text-muted-foreground">
+					Choose your new password for your account
+				</p>
+			</div>
+			<div className="grid gap-6">
+				<div className="grid gap-2">
+					<div className="flex items-center">
+						<Label htmlFor="password">Password</Label>
+					</div>
+					<Input id="password" type="password" required />
+				</div>
+                <div className="grid gap-2">
+					<div className="flex items-center">
+						<Label htmlFor="password">Confirm Password</Label>
+					</div>
+					<Input id="password" type="password" required />
+				</div>
+				<Button type="submit" className="w-full">
+					Confirm
+				</Button>
+			</div>
+			
+		</form>
+	);
+}
---
+++ src/app/(public)/verifyotpUpdate/page.tsx
@@ -0,0 +1,36 @@
+import { LoginForm } from "@/app/(public)/_components/login-form";
+import Image from "next/image";
+import VerifyOtoConfirmPage from "../verifyOTPConfirm/page";
+import { VerifyotpUpdateConfirmForm } from "../_components/verifyotpUpdateConfirm";
+export default function verifyotpUpdateConfirmPage() {
+    return (
+        <div className="grid min-h-svh lg:grid-cols-2">
+            <div className="flex flex-col gap-4 p-6 md:p-10">
+                <div className="flex justify-center gap-2 md:justify-start">
+                    <Image
+                        src="/logo.png"
+                        alt="logo"
+                        width={100}
+                        height={100}
+                        className="object-cover"
+                    />
+                </div>
+                <div className="flex flex-1 items-center justify-center">
+                    <div className="w-full max-w-lg">
+                        <VerifyotpUpdateConfirmForm />
+                    
+                        
+                    </div>
+                </div>
+            </div>
+            <div className="relative hidden lg:flex justify-start items-center ml-20">
+                <Image
+                    src="/verify 2.png"
+                    alt="Verify-Image"
+                    width={600}
+                    height={600}
+                />
+            </div>
+        </div>
+    );
+}
