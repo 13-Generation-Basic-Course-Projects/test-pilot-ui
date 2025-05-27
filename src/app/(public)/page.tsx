133087763f451e3c15920816b13b9886facc01ee
Sokkeang123 <sokkeangsieb27225@gmailc.com>
2025-05-27 09:59:42 +0700

    feat(invite-to-project): create popup of invite to project

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,9 @@
+import { InviteToProject } from "@/components/invite-to-projecct";
 
 export default function Home() {
 	return (
 		<div className="ps-80">
-			
+			<InviteToProject/>
 		</div>
 	);
 }
