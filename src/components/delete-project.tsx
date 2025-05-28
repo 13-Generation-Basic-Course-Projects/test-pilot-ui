3d15f1b6a76f2341a7ac3e171f1492e1246945d8
Somphors10 <srornchansomphors@gmail.com>
2025-05-28 17:04:38 +0700

    feat(all popup): delete project

---
+++ src/components/delete-project.tsx
@@ -0,0 +1,47 @@
+// DeleteProject.tsx
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { ProjectItem } from "@/types";
+
+type DeleteProjectProps = {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  project: ProjectItem | null;
+  onDeleteConfirm: (projectId: string) => void;
+};
+
+export function DeleteProject({
+    
+  open,
+  onOpenChange,
+  project,
+  onDeleteConfirm,
+}: DeleteProjectProps) {
+  if (!project) return null; //Prevents the dialog from rendering if no project is selected.
+  return (
+    <AlertDialog open={open} onOpenChange={onOpenChange}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This will permanently delete the project <strong>{project.title}</strong>. This action cannot be undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={() => onDeleteConfirm(project.id)}>
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
