ea83120b5968cdd51b33660fcf3bb6a43fa1cd0a
Somphors10 <srornchansomphors@gmail.com>
2025-06-20 13:39:07 +0700

    feat(fetch-share-endpoint): fetch share project

---
+++ src/app/api/v1/publicShareLink/verify/page.tsx
@@ -0,0 +1,6 @@
+import VerifyForm from '@/components/verify-share-project'
+import { Suspense } from 'react'
+
+export default function pages() {
+  return <Suspense><VerifyForm /></Suspense>
+}
--- src/app/api/v1/publicShareLink/verify/pages.tsx
+++
@@ -1,5 +0,0 @@
-import VerifyForm from '@/components/verify-share-project'
-
-export default function page() {
-  return <VerifyForm/>
-}
